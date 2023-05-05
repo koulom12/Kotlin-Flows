ee7896fed9ab4d388859a662563090fc3e605435
omeshkoul <koulom12@gmail.com>
2023-05-06 02:24:21 +0530

    State Flow

--- app/src/main/java/com/example/flowsbasics/MainActivity.kt
+++ app/src/main/java/com/example/flowsbasics/MainActivity.kt
@@ -3,44 +3,34 @@ package com.example.flowsbasics
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.flowsbasics.ui.theme.FlowsBasicsTheme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    val channel = Channel<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.Main).launch {
-            producer()
-            consumer()
+        GlobalScope.launch() {
+            val result = producer()
+            delay(4000)
+            result.collect {
+                Log.d("Flow Value 1", it.toString())
+            }
         }
     }
 
-    private fun producer() {
-        CoroutineScope(Dispatchers.Main).launch {
-            channel.send(1)
-            channel.send(2)
+    private fun producer() : StateFlow<Int> {
+        val mutableStateFlow = MutableStateFlow(10)
+        GlobalScope.launch {
+            delay(2000)
+            mutableStateFlow.emit(20)
+            delay(2000)
+            mutableStateFlow.emit(30)
         }
+        return mutableStateFlow
     }
-
-    private fun consumer() {
-        CoroutineScope(Dispatchers.Main).launch {
-            Log.d("Channel", channel.receive().toString())
-            Log.d("Channel", channel.receive().toString())
-        }
-    }
-
 }
